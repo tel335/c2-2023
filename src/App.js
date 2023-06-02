83060a337955bd2dcdc7c10612c02e9b0ab7fcf8
Enrique <100406561@alumnos.uc3m>
2023-06-02 15:03:05 -0400

    commit final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import Case_1_2_3 from './components/case_1_2_3'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>CONTROL 2 TEL-335</h1>
+    <div className="App" style={{marginTop:40}}>
+      <h1>CONTROL 2 TEL-335 - Enrique Pérez Herranz</h1>
+      <Case_1_2_3 />
     </div>
   );
 }
